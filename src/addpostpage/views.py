888914520e9c95ad9c015cfb35bf5871242c45d3
anurag199831 <anuragchaudhary31@gmail.com>
2019-10-29 05:19:39 +0530

    added blog input from page

--- src/addpostpage/views.py
+++ src/addpostpage/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from homepage.models import Blog
+from homepage.models import Category,Blog
 # Create your views here.
 
 def index(request):
-    return render(request,'addpost.html')
+    catg= Category.objects.all()
+    params={'cat':catg}
+    return render(request,'addpost.html',params)
 
 def postsubmit(request):
     print('hello')
+    mycategory=request.GET.get('categ','no value')
     mytitle=request.GET.get('title','no value')
     mycontent=request.GET.get('content','no value')
-    print(mytitle,mycontent)
-    temp=Blog(mytitle,1,mycontent)
+    print(mytitle,mycontent,mycategory)
+    Blog.objects.create(title=mytitle,category=mycategory,content= mycontent)
     return HttpResponse("submitted")
--- src/homepage/models.py
+++ src/homepage/models.py
@@ -4,9 +4,8 @@ from django.db import models
 
 class Category(models.Model):
     name=models.CharField(max_length=50)
-    description=models.CharField(max_length=1000)
 
 class Blog(models.Model):
     title=models.CharField(max_length=100)
-    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    category=models.CharField(max_length=50)
     content=models.CharField(max_length=10000)
